3cedad90189d7338c95ece54cf96adc22e2dabd8
NaRan <narayanpanthi@MacBook-Air.local>
2025-04-24 18:23:12 +0545

    login status added form shared pref

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:butterfly/core/database/hive_initalizer.dart';
 import 'package:butterfly/core/di/di_module.dart';
+import 'package:butterfly/core/preference/pref_manager.dart';
 import 'package:butterfly/navigation/router.dart';
 import 'package:butterfly/utils/app_logger.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initHive();
   await configureDependenciesInjection();
-  final loggedInStatus = false;
+  final preferenceManager = getIt<PreferenceManager>();
+  final bool loggedInStatus = preferenceManager.getLoggedIn();
   AppLogger.configureLogging();
   runApp(MyApp(isLoggedIn: loggedInStatus));
 }
