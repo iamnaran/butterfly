89cc8811463fc3f31861527bb9cc216c8c60ba30
NaRan <narayanpanthi@MacBook-Air.local>
2025-05-02 01:52:32 +0545

    minor fixes

--- lib/core/mqtt/mqtt_manager.dart
+++ lib/core/mqtt/mqtt_manager.dart
@@ -2,6 +2,8 @@
 
 import 'dart:async';
 import 'dart:io';
+import 'dart:convert';
+import 'package:butterfly/utils/app_logger.dart';
 import 'package:flutter/foundation.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
@@ -14,10 +16,6 @@ class MQTTConnection {
   String? password;
   String? token;
   String? sessionId;
-
-  String? caCertPath;
-  String? clientCertPath;
-  String? clientKeyPath;
 
   final MqttQos defaultQos;
 
@@ -60,47 +58,55 @@ class MQTTConnection {
   final Duration _reconnectDelay = const Duration(seconds: 5);
 
   Future<void> connect() async {
+    debugPrint('MQTT client Connect Called');
+
     _connectionStatusController.add(MqttConnectionState.connecting);
 
-    _client = MqttServerClient.withPort(broker!, clientId!, 1883);
+    // _client = MqttServerClient.withPort(broker!, clientId!, 1883);
 
-    final MqttConnectMessage connMess = MqttConnectMessage()
-        .withClientIdentifier(clientId!)
-        .withWillQos(MqttQos.atLeastOnce)
-        .startClean();
+    _client = MqttServerClient('test.mosquitto.org', 'client-idflutter_client_xasasa');
+    _client!.port = 1883;
+
+    // final MqttConnectMessage connMess = MqttConnectMessage()
+    //     .withClientIdentifier(clientId!)
+    //     .withWillQos(MqttQos.atLeastOnce)
+    //     .startClean();
 
     // ack msg
 
-    if (username != null && password != null) {
-      connMess.authenticateAs(username!, password!);
-    }
+    // if (username != null && password != null) {
+    //   connMess.authenticateAs(username!, password!);
+    // }
 
-    if (caCertPath != null && clientCertPath != null && clientKeyPath != null) {
-      try {
-        final caCert = await getHardcodedCaCert();
-        final clientCert = await getHardcodedClientCert();
-        final clientKey = await getHardcodedClientKey();
+    // try {
+    //   final caCert = await getCaCert();
+    //   final clientCert = await getClientCert();
+    //   final clientKey = await getClientKey();
 
-        if (caCert == null || clientCert == null || clientKey == null) {
-          throw Exception('Certificate or key is missing.');
-        }
+    //   if (caCert == null || clientCert == null || clientKey == null) {
+    //     throw Exception('Certificate or key is missing.');
+    //   }
+    //   // print ca cert
+    //   debugPrint('CA Cert: $caCert');
+    //   debugPrint('Client Cert: $clientCert');
+    //   debugPrint('Client Key: $clientKey');
 
-        final securityContext = SecurityContext(withTrustedRoots: true)
-          ..setClientAuthoritiesBytes(caCert.codeUnits)
-          ..useCertificateChainBytes(clientCert.codeUnits)
-          ..usePrivateKeyBytes(clientKey.codeUnits)
-          ..setTrustedCertificatesBytes(caCert.codeUnits)
-          ..minimumTlsProtocolVersion = TlsProtocolVersion.tls1_2;
-
-        _client!.securityContext = securityContext;
-      } catch (e) {
-        debugPrint('Error loading certificates: $e');
-        _connectionStatusController.add(MqttConnectionState.faulted);
-        return;
-      }
-    }
+      // final securityContext = SecurityContext(withTrustedRoots: true)
+      //   ..setTrustedCertificates()
+      //   ..setClientAuthorities(utf8.encode(caCert))
+      //   ..useCertificateChain(utf8.encode(clientCert))
+      //   ..usePrivateKey())
+    
+    //   _client!.securityContext = securityContext;
+    // } catch (e) {
+    //   debugPrint('Error loading certificates: $e');
+    //   _connectionStatusController.add(MqttConnectionState.faulted);
+    //   return;
+    // }
 
     _client!.logging(on: true);
+    _client!.secure = false;
+    _client!.keepAlivePeriod = 60;
     _client!.setProtocolV311();
 
     _client!.onConnected = () {
@@ -127,9 +133,12 @@ class MQTTConnection {
       }
     };
 
-    _client!.connectionMessage = connMess; // Assign the connect message
+    // _client!.connectionMessage = connMess; // Assign the connect message
 
     try {
+
+      // print broker url, port
+      debugPrint('Connecting to broker NEW : $broker, port: 1883');
       await _client!.connect();
     } on NoConnectionException catch (e) {
       if (!_connectionStatusController.isClosed) {
@@ -213,27 +222,55 @@ class MQTTConnection {
     }
   }
 
-  static Future<String?> getHardcodedCaCert() async {
+// ca cert
+
+  static Future<String?> getCaCert() async {
     return """
-  -----BEGIN CERTIFICATE-----
-  ...YOUR CERT...
-  -----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+MIIDEzCCAfugAwIBAgIBAjANBgkqhkiG9w0BAQsFADA/MQswCQYDVQQGEwJDTjER
+MA8GA1UECAwIaGFuZ3pob3UxDDAKBgNVBAoMA0VNUTEPMA0GA1UEAwwGUm9vdENB
+MB4XDTIwMDUwODA4MDcwNVoXDTMwMDUwNjA4MDcwNVowPzELMAkGA1UEBhMCQ04x
+ETAPBgNVBAgMCGhhbmd6aG91MQwwCgYDVQQKDANFTVExDzANBgNVBAMMBlNlcnZl
+cjCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALNeWT3pE+QFfiRJzKmn
+AMUrWo3K2j/Tm3+Xnl6WLz67/0rcYrJbbKvS3uyRP/stXyXEKw9CepyQ1ViBVFkW
+Aoy8qQEOWFDsZc/5UzhXUnb6LXr3qTkFEjNmhj+7uzv/lbBxlUG1NlYzSeOB6/RT
+8zH/lhOeKhLnWYPXdXKsa1FL6ij4X8DeDO1kY7fvAGmBn/THh1uTpDizM4YmeI+7
+4dmayA5xXvARte5h4Vu5SIze7iC057N+vymToMk2Jgk+ZZFpyXrnq+yo6RaD3ANc
+lrc4FbeUQZ5a5s5Sxgs9a0Y3WMG+7c5VnVXcbjBRz/aq2NtOnQQjikKKQA8GF080
+BQkCAwEAAaMaMBgwCQYDVR0TBAIwADALBgNVHQ8EBAMCBeAwDQYJKoZIhvcNAQEL
+BQADggEBAJefnMZpaRDHQSNUIEL3iwGXE9c6PmIsQVE2ustr+CakBp3TZ4l0enLt
+iGMfEVFju69cO4oyokWv+hl5eCMkHBf14Kv51vj448jowYnF1zmzn7SEzm5Uzlsa
+sqjtAprnLyof69WtLU1j5rYWBuFX86yOTwRAFNjm9fvhAcrEONBsQtqipBWkMROp
+iUYMkRqbKcQMdwxov+lHBYKq9zbWRoqLROAn54SRqgQk6c15JdEfg
+-----END CERTIFICATE-----
   """;
   }
 
-  static Future<String?> getHardcodedClientCert() async {
-    return """
-  -----BEGIN CERTIFICATE-----
-  ...YOUR CLIENT CERT...
-  -----END CERTIFICATE-----
-  """;
-  }
+// client key
 
-  static Future<String?> getHardcodedClientKey() async {
+  static Future<String?> getClientKey() async {
     return """
   -----BEGIN PRIVATE KEY-----
-  ...YOUR PRIVATE KEY...
-  -----END PRIVATE KEY-----
+MIIEpAIBAAKCAQEA7yhhQzfgTYV79Y3FlhQ93lD5e9fh+6tQfXql5w2/5kdmVG/J
+X2A2Ay7B/52EjPB7xWErv3lbksJ6ApIMffkaEXeO3U5cuHaQJkcnx1dYIifEKXsY
+s82cLTFpz/Kq6sqjttSfu2w0gt2nxaNKjxUV+g==
+-----END PRIVATE KEY-----
+  """;
+  }
+
+// client cert
+  static Future<String?> getClientCert() async {
+    return """
+-----BEGIN CERTIFICATE-----
+MIIDEzCCAfugAwIBAgIBATANBgkqhkiG9w0BAQsFADA/MQswCQYDVQQGEwJDTjER
+MA8GA1UECAwIaGFuZ3pob3UxDDAKBgNVBAoMA0VNUTEPMA0GA1UEAwwGUm9vdENB
+MB4XDTIwMDUwODA4MDY1N1oXDTMwMDUwNjA4MDY1N1owPzELMAkGA1UEBhMCQ04x
+ETAPBgNVBAgMCGhhbmd6aG91MQwwCgYDVQQKDANFTVExDzANBgNVBAMMBkNsaWVu
+dDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMy4hoksKcZBDbY680u6
+TS25U51nuB1FBcGMlF9B/t057wPOlxF/OcmbxY5MwepS41JDGPgulE1V7fpsXkiW
+1LUimYV/tsqBfymIe0mlY7oORahKji7zKQ2UBIVFhdlvQxunlIDnw6F9popUgyHt
+dMhtlgZK8oqRwHxO5dbfoukYd6J/r+etS5q26sgVkf3C6dt0Td7B25H9qW+f7oLV
+-----END CERTIFICATE-----
   """;
   }
 
